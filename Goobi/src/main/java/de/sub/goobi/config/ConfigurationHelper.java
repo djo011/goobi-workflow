2813aec254e8edaa7330ae796f496a0934011731
djo011 <djo011@uib.no>
2024-01-19 13:34:49 +0100

    add getters for OIDC in configHelper

--- Goobi/src/main/java/de/sub/goobi/config/ConfigurationHelper.java
+++ Goobi/src/main/java/de/sub/goobi/config/ConfigurationHelper.java
@@ -653,13 +653,25 @@ public class ConfigurationHelper implements Serializable {
     public boolean isOIDCAutoRedirect() {
         return getLocalBoolean("OIDCAutoRedirect", false);
     }
-
+    
+    public String getOIDCFlowType() {
+        return getLocalString("OIDCFlowType", "");
+    }
+    
     public String getOIDCAuthEndpoint() {
         return getLocalString("OIDCAuthEndpoint", "");
     }
 
     public String getOIDCLogoutEndpoint() {
         return getLocalString("OIDCLogoutEndpoint", "");
+    }
+    
+    public String getOIDCTokenEndpoint() {
+        return getLocalString("OIDCTokenEndpoint", "");
+    }
+    
+    public String getOIDCHostName() {
+        return getLocalString("OIDCHostName", "");
     }
 
     public String getOIDCIssuer() {
@@ -672,6 +684,10 @@ public class ConfigurationHelper implements Serializable {
 
     public String getOIDCClientID() {
         return getLocalString("OIDCClientID", "");
+    }
+    
+    public String getOIDCClientSecret() {
+        return getLocalString("OIDCClientSecret", "");
     }
 
     public String getOIDCIdClaim() {
